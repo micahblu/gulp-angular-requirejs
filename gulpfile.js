bcb7065a603f3e214bf72fee8fdc7a8ba09a423a
Micah Blu <micahblu@gmail.com>
2015-07-08 10:28:14 -0700

    Added dist task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var gulp = require('gulp'),
+    concatCss = require('gulp-concat-css'),
     connect = require('gulp-connect'),
-    sass = require('gulp-sass');
+    minifyCss = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
   connect.server({
     root: 'src',
     livereload: true
   });
+});
+
+gulp.task('copy-html', function() {
+  gulp.src('./src/**/*.html')
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy-lib', function() {
+  gulp.src('./src/lib/**/*')
+  .pipe(gulp.dest('./dist/lib/'));
+});
+
+gulp.task('minify-css', function() {
+  gulp.src('./src/styles/*.css')
+  .pipe(concatCss('main.css'))
+  .pipe(minifyCss({compatibility: 'ie8'}))
+  .pipe(gulp.dest('./dist/styles/'));
 });
 
 gulp.task('reload', function() {
@@ -26,4 +46,12 @@ gulp.task('watch', function() {
   gulp.watch(['./src/styles/**/*.scss'], ['sass', 'reload']);
 });
 
+gulp.task('uglify', function() {
+  return gulp.src('./src/scripts/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts/'));
+});
+
 gulp.task('default', ['connect', 'watch']);
+
+gulp.task('dist', ['uglify', 'copy-lib', 'minify-css', 'copy-html']);
